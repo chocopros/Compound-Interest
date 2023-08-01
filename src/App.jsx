dc83958730cd0cd2bcbd6c1022e722e6197299c8
jesuschock93 <jesuschock93@gmail.com>
2023-08-01 10:13:38 -0400

    form&input

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components'
+import { Formik, Form } from 'formik'
+import Input from './Components/Input/Input';
+
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  align-items: center;
+`;
+
+const Section = styled.section`
+  background-color: #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width:500px;
+  box-shadow: 0px 2px 3px rgb(0,0,0,0.3)
+`;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleSubmit = () => {
+
+  };
+  
+  
 
   return (
     <>
+      <Container>
+        <Section>
 
+          <Formik
+            initialValues={{
+              deposit: '',
+              contribution: '',
+              years: '',
+              rate: ''
+            }}
+            onSubmit={handleSubmit}
+          >
+            
+            <Form>
+              <Input name="deposit" label="Deposito inicial" />
+              <Input name="contribution" label="Contribucion anual" />
+              <Input name="years" label="Años" />
+              <Input name="rate" label="Interes Estimado" />
+            </Form>
+          </Formik>
+
+        </Section>
+      </Container>
     </>
   )
-}
+};
 
 export default App
